b13d3cc2eb05e8df40a77b807e7b35b20682539e
Edmund Miller <edmund.a.miller@protonmail.com>
2021-04-20 16:59:05 -0500

    build: Add flake

---
+++ flake.nix
@@ -0,0 +1,14 @@
+{
+  description = "A flake for building my CV";
+  # Provides abstraction to boiler-code when specifying multi-platform outputs.
+  inputs.flake-utils.url = "github:numtide/flake-utils";
+  outputs = { self, nixpkgs, flake-utils }:
+    flake-utils.lib.eachDefaultSystem (system:
+      let pkgs = nixpkgs.legacyPackages.${system};
+      in {
+        devShell = pkgs.mkShell {
+          name = "CV";
+          buildInputs = with pkgs; [ texlive.combined.scheme-full ];
+        };
+      });
+}
